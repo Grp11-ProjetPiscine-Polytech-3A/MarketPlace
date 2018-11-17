8d2e971884b50b528d5801756240f655579cebb8
Unknown <laurancy.dorian@gmail.com>
2018-11-17 14:37:30 +0100

    Cryptage des mots de passe pour la connexion

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,25 +1,40 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-class User_model extends MY_Model
-{
-	// Nom de la table
-	protected $table = 'user';
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class User_model extends MY_Model {
+
+    // Nom de la table
+    protected $table = 'user';
     // Nom de l'identifiant de la table
     protected $id = '';
+
+    
+//    public function create($options_echappees = array()) {
+//        
+//    }
 
     /**
      * Check the user with the username and password sent by the user
      * @param   Array     $data    the associative array that contains the loginUser and passUser
      * @return  boolean            TRUE if the informations given are correct, FALSE else
      */
-    public function login($data) {
-        // Makes the request        
+    public function login($login, $password) {
+        // Encrypt the password
+        $encrypted_password = encrypt_password($password, $login);
+
+        // Makes the request
+        $data = array(
+            "loginUser" => $login,
+            "passUser" => $encrypted_password,
+        );
+
         $this->db->select('*');
         $this->db->from('User');
-        $this->db->where('loginUser',$data['loginUser']);
-        $this->db->where('passUser',$data['passUser']);
+        $this->db->where($data);
         $this->db->limit(1);
-        
+
         // Executes the query
         $query = $this->db->get();
 
@@ -47,8 +62,8 @@ class User_model extends MY_Model
             return false;
         }
     }
-}
 
+}
 
 /* End of file produit_variante_model.php */
 /* Location: ./application/models/produit_variante_model.php */
