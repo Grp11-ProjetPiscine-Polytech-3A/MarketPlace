e5301702e48741aa158e6bbe30a6a132c0e16b7d
Unknown <laurancy.dorian@gmail.com>
2018-11-18 16:19:04 +0100

    Changement de la méthode de cryptage des données

--- application/helpers/encrypt_helper.php
+++ application/helpers/encrypt_helper.php
@@ -15,32 +15,16 @@ if (!function_exists('encrypt_string')) {
      * @return String   The encrypted string
      */
     function encrypt_string($pure_string, $encryption_key) {
-        $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
-        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-        $encrypted_string = mcrypt_encrypt(MCRYPT_BLOWFISH, $encryption_key, utf8_encode($pure_string), MCRYPT_MODE_ECB, $iv);
+        $textToEncrypt = $pure_string;
+        $encryptionMethod = "AES-256-CBC";  // AES is used by the U.S. gov't to encrypt top secret documents.
+        $secretHash = $encryption_key;
+
+        $encrypted_string = openssl_encrypt($textToEncrypt, $encryptionMethod, $secretHash, 0, "56897ngq56897ngq");
         return $encrypted_string;
     }
 
 }
 
-if (!function_exists('decrypt_string')) {
-
-// Sources www.stackoverflow.com
-
-    /**
-     * Decrypt a string
-     * @param String $encrypted_string  The encrypted string
-     * @param String $encryption_key    The encryption key
-     * @return String   The decrypted String
-     */
-    function decrypt_string($encrypted_string, $encryption_key) {
-        $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
-        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-        $decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, $encryption_key, $encrypted_string, MCRYPT_MODE_ECB, $iv);
-        return $decrypted_string;
-    }
-
-}
 
 if (!function_exists('encrypt_password')) {
 
@@ -54,6 +38,7 @@ if (!function_exists('encrypt_password')) {
         $key = "rLY5eo2Cf5";    // Change this key when installing, you can find one at https://randomkeygen.com
         $salt = encrypt_string($login, $key);
         $pw_before_hash = $salt . $password;
+        
         return sha1($pw_before_hash);
     }
 
