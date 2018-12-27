ce4626ab0ee4051324dc2c99332ed236015fc91f
Laurancy-dorian <laurancy.dorian@gmail.com>
2018-12-27 13:07:40 +0100

    FEATURE (function assets) : Ajout d'une fonction url_images_in_folder

--- application/helpers/assets_helper.php
+++ application/helpers/assets_helper.php
@@ -70,3 +70,28 @@ if (!function_exists('url_files_in_folder')) {
     }
 
 }
+
+if (!function_exists('url_images_in_folder')) {
+
+    /**
+     * Return an array of urls of the images in a folder given in parameter
+     * @param String $folder    The path to the folder (relative to the root of the website)
+     */
+    function url_images_in_folder($folder) {
+        $path = FCPATH . $folder;
+        $files_url = array();
+
+        if (is_dir($path)) {
+            $files = scandir($path);
+            for ($i = 2; $i < count($files); $i++) {
+                if (!is_dir($path . '/' . $files[$i])) {
+                    $files_url[] = base_url($folder . "/" . $files[$i]);
+                }
+            }
+        }
+        if (count($files_url) == 0) {
+            $files_url[] = base_url("/assets/images/no-image.png");
+        }
+        return $files_url;
+    }
+}
