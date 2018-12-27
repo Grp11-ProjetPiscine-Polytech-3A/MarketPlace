e7f822bc246dc02dbeeee1672b2a5b14fb5ccd19
Raph <raphael.oziol@hotmail.fr>
2018-12-27 15:36:57 +0100

    FEATURE (liste_commerce): affichage des commerces d'un commercant

---
+++ application/views/Commercant/Commerces/liste_commerces.php
@@ -0,0 +1,40 @@
+<div id="liste_commerces">
+
+    <div class="row">
+
+        <?php foreach ($commerces as $c): ?>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="<?php echo site_url('/Commerces/fiche_commerce/' . $c->siretCommerce) ?>">
+                                <?php echo $c->nomCommerce ?>
+                            </a>
+                        </h4>
+                        <p class="card-text">
+                            <?php echo $c->mailCommerce ?>
+                            <br/>
+                            <?php echo $c->telCommerce ?>
+                            <br/>
+                            <?php echo $c->numAdresseCommerce ?>
+                            <?php echo $c->rueCommerce ?>
+                            <br/>
+
+                            <?php
+                            if ($c->complementAdresseCommerce)
+                                echo $c->complementAdresseCommerce . '<br/>';
+                            ?>
+
+                            <?php echo $c->codePostalCommerce ?>
+                            <?php echo $c->villeCommerce ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
+
+    </div>
+
+
+</div>
