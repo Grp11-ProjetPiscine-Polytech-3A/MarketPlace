6113d8e57d9d5579e20cc193e7d0aaf945389574
PepitoSama <etienne.saimond@gmail.com>
2019-01-06 10:36:04 +0100

    Ajout de la vérification du tableau de produit passé en paramètre : Il ne doit pas être vide

--- application/views/Produits/liste_produits.php
+++ application/views/Produits/liste_produits.php
@@ -1,30 +1,26 @@
 <div id="liste_produits">
 
     <div class="row">
-
-        <?php foreach ($produits as $p): ?>
-            <div class="col-lg-4 col-md-6 mb-4">
-                <div class="card h-100">
-                    <a  class="border-bottom" href="<?php echo site_url('/Produits/fiche_produit/' . $p->idProduitType) ?>" style="display:flex; justify-content: center">
-                        <img class="card-img-top" src="<?php echo $p->img_url ?>" alt="<?php echo $p->nomProduitType ?>" style="height:300px; width: auto;">
-                    </a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="<?php echo site_url('/Produits/fiche_produit/' . $p->idProduitType) ?>">
-                                <?php echo $p->nomProduitType ?>
-                            </a>
-                        </h4>
-                        <h5><?php echo $p->prixProduitType ?> €</h5>
-                        <p class="card-text"><?php echo $p->descriptionProduitType ?></p>
+        <?php if(isset($produits)): ?>
+            <?php foreach ($produits as $p): ?>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card h-100">
+                        <a  class="border-bottom" href="<?php echo site_url('/Produits/fiche_produit/' . $p->idProduitType) ?>" style="display:flex; justify-content: center">
+                            <img class="card-img-top" src="<?php echo $p->img_url ?>" alt="<?php echo $p->nomProduitType ?>" style="height:300px; width: auto;">
+                        </a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                <a href="<?php echo site_url('/Produits/fiche_produit/' . $p->idProduitType) ?>">
+                                    <?php echo $p->nomProduitType ?>
+                                </a>
+                            </h4>
+                            <h5><?php echo $p->prixProduitType ?> €</h5>
+                            <p class="card-text"><?php echo $p->descriptionProduitType ?></p>
+                        </div>
                     </div>
-                    <!--                    <div class="card-footer">
-                                            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                                        </div>-->
                 </div>
-            </div>
-
-        <?php endforeach; ?>
-
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
 
 
