66e4f8cabb8ccbf5b2121de4594f1ae76caa1a00
Raph <raphael.oziol@hotmail.fr>
2019-01-07 18:02:54 +0100

    ajout des views pour l'ajout de boutons d'ajout

---
+++ application/views/Commerces/display_ajout_commerce.php
@@ -0,0 +1,7 @@
+
+<div id="ajout_commerce" class="col-12 container mb-4 mt-2">
+    <a class="btn btn-outline-success" href="<?php echo site_url('/Commercant/Produits/ajout_produit') ?>">
+        Ajouter un produit
+    </a>
+</div>
+    
